36158065ca4ba4a1b28ae82be27a99264fa512b9
Steve NOSSE <nossesteve@gmail.com>
2023-10-24 17:05:19 +0200

    Hide RSOD in release mode.

--- lib/src/core/app_initializer.dart
+++ lib/src/core/app_initializer.dart
@@ -1,7 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
 class AppInitializer {
   /// Initialize services, plugins, etc. before the app runs.
   Future<void> preAppRun() async {}
 
   /// Initialize services, plugins, etc. after the app runs.
-  Future<void> postAppRun() async {}
+  Future<void> postAppRun() async {
+    // Hide RSOD in release mode.
+    if (kReleaseMode) {
+      ErrorWidget.builder = (FlutterErrorDetails details) => const SizedBox();
+    }
+  }
 }
