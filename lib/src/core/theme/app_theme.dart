d81c1822c04abb3b64428b8b7ae2d3cc302c0ac4
Steve NOSSE <nossesteve@gmail.com>
2025-02-02 21:19:49 +0100

    update theme to take colors into account

--- lib/src/core/theme/app_theme.dart
+++ lib/src/core/theme/app_theme.dart
@@ -17,6 +17,7 @@ class AppTheme {
       ),
       cardTheme: CardTheme(
         color: colors.surface,
+        shadowColor: colors.shadow.withValues(alpha: .5),
       ),
       colorScheme: colors,
       extensions: [brightness == Brightness.light ? CustomColors.light : CustomColors.dark],
@@ -24,51 +25,72 @@ class AppTheme {
   }
 
   static TextTheme _buildTextTheme({required ColorScheme colors}) {
-    return const TextTheme(
+    return TextTheme(
       displayLarge: TextStyle(
         fontFamily: _fontFamily,
         fontWeight: FontWeight.w700,
         fontSize: 32,
+        color: colors.onSurface,
       ),
       headlineLarge: TextStyle(
         fontFamily: _fontFamily,
         fontWeight: FontWeight.w700,
         fontSize: 28,
+        color: colors.onSurface,
       ),
       titleLarge: TextStyle(
         fontFamily: _fontFamily,
         fontWeight: FontWeight.bold,
         fontSize: 24,
+        color: colors.onSurface,
       ),
       titleMedium: TextStyle(
         fontFamily: _fontFamily,
         fontWeight: FontWeight.w700,
         fontSize: 20,
+        color: colors.onSurface,
       ),
       titleSmall: TextStyle(
         fontFamily: _fontFamily,
         fontWeight: FontWeight.w700,
         fontSize: 18,
+        color: colors.onSurface,
       ),
       bodyLarge: TextStyle(
         fontFamily: _fontFamily,
         fontWeight: FontWeight.w600,
         fontSize: 16,
+        color: colors.onSurface,
       ),
       bodyMedium: TextStyle(
         fontFamily: _fontFamily,
         fontWeight: FontWeight.w500,
         fontSize: 16,
+        color: colors.onSurface,
       ),
       bodySmall: TextStyle(
         fontFamily: _fontFamily,
         fontWeight: FontWeight.w500,
         fontSize: 14,
+        color: colors.onSurface,
+      ),
+      labelLarge: TextStyle(
+        fontFamily: _fontFamily,
+        fontWeight: FontWeight.w500,
+        fontSize: 14,
+        color: colors.onSurface,
+      ),
+      labelMedium: TextStyle(
+        fontFamily: _fontFamily,
+        fontWeight: FontWeight.w500,
+        fontSize: 14,
+        color: colors.onSurface,
       ),
       labelSmall: TextStyle(
         fontFamily: _fontFamily,
         fontWeight: FontWeight.w500,
         fontSize: 10,
+        color: colors.onSurface,
       ),
     );
   }
